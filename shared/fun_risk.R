708e1ac2fe1b017cf46014ff276be238238e8367
jjcurtin <jjcurtin@wisc.edu>
2021-07-27 21:05:53 -0500

    Update to lapse coding

--- shared/fun_risk.R
+++ shared/fun_risk.R
@@ -81,34 +81,28 @@ merge_lapses <- function(lapses) {
 }
 
 
-get_lapse_hours <- function(subid, study_start, study_end, ema_end) {
+get_study_hours <- function(subid, study_start, study_end, ema_end) {
   # Returns a tibble for one subject with columns for subid (numeric) and 
   #   lapse_hour (dttm).  
   #   First hour is midnight on study day 2
-  #   Last hour is earlier of the hour of the final EMA or 11 pm on the last day
+  #   Last hour is earlier of the hour preceding the final EMA or 11 pm on the last day
   #   of the study
   # Inputs:
-  #   all date or dttm in America/Chicago
+  #   all dttm in America/Chicago
   
-  hour_start <- study_start %>% 
-    as_datetime() %>% # forces as UTC
-    force_tz(tz = "America/Chicago") # set to America/Chicago with same hour
+  hour_start <- study_start + days(1)
   
   # calculate hour_end in three steps
-  study_end <- study_end %>% 
-    as_datetime() %>% # forces as UTC
-    force_tz(tz = "America/Chicago") %>%   # set to America/Chicago with same hour
-    `+`(hours(23))  # 11 pm on study_end date
+  study_end <- study_end + (hours(23))  # 11 pm on study_end date
   
-  ema_end <- ema_end %>% 
-    floor_date(unit = "hours")
+  ema_end <-  floor_date(ema_end, unit = "hours") - hours(1)
   
   hour_end <- min(study_end, ema_end)
   
-  lapse_hours <- tibble(hour = seq(hour_start, hour_end, by = "hour"), subid) %>% 
+  study_hours <- tibble(hour = seq(hour_start, hour_end, by = "hour"), subid) %>% 
     relocate(subid)
   
-  return(lapse_hours)
+  return(study_hours)
 }
 
 get_lapse_labels <- function(lapses, dates) {
@@ -117,27 +111,47 @@ get_lapse_labels <- function(lapses, dates) {
   
   labels <- dates %>% 
     select(subid, study_start, study_end, ema_end) %>% 
-    pmap_dfr(~get_lapse_hours(..1, ..2, ..3, ..4)) %>% 
+    pmap_dfr(~get_study_hours(..1, ..2, ..3, ..4)) %>% 
     mutate(lapse = FALSE,
            no_lapse = TRUE)
   
   
+  # first handle lapses
   valid_lapses <- lapses %>% 
     filter(!exclude)
   
+  no_match <- 0
   for (i in 1:nrow(valid_lapses)) {
     lapse_subid <- valid_lapses$subid[[i]]
     lapse_hour <- valid_lapses$lapse_start[[i]]
     
     row_index <- which(labels$subid == lapse_subid & labels$hour == lapse_hour)
     
-    if (length(row_index == 1)) {
+    if (length(row_index) == 1) {
       labels$lapse[row_index] <- TRUE
-    } else{
-      message(i)
+    } 
+    
+    # some extra checks
+    # this should not happen
+    if (length(row_index) > 1) {
+      stop("get_lapse_labels() multiple match; SubID: ", lapse_subid, " Lapse hour: ", lapse_hour, " i: ", i)
+    }
+    
+    # this can happen if we have a lapse report outside of the study_hours
+    if (length(row_index) == 0) {
+      warnings("get_lapse_labels() no match; SubID: ", lapse_subid, " Lapse hour: ", lapse_hour, " i: ", i)
+      no_match <- no_match + 1
     }
   }
-
+  
+  # final check for lapses
+  if (! (nrow(valid_lapses) == (sum(labels$lapse) + no_match))) {
+    stop("No all lapses accounted")
+  }
   
   
-}
+  # now handle no_lapse exclusions
+  exclusions <-  lapses %>% 
+    filter(!exclude)
+  
+} 
