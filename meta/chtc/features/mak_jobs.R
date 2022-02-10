845de1705a287a9b36a4ae1621cebbe870b6f836
Kendra Wyant <kpaquette2@wisc.edu>
2022-02-10 08:35:48 -0600

    writes out CHTC specific training controls during mak_jobs

--- meta/chtc/features/mak_jobs.R
+++ meta/chtc/features/mak_jobs.R
@@ -5,13 +5,13 @@ suppressPackageStartupMessages({
 })
 
 # CHANGE ME
-name_job <- "features_1_day"
+name_job <- "features_1_hour"
 path_jobs <- "P:/studydata/risk/chtc/meta/jobs/features/"
 path_templates <- "./meta/chtc/features/templates"
 path_data <- "P:/studydata/risk/data_processed/meta" 
 path_fun <- "./shared/fun_chtc_features.R"
 fun_name <- "fun_chtc_features.R" # this will be name that function appears as on CHTC
-labels_file_name <- "labels_1_day.rds"
+labels_file_name <- "labels_1_hour.rds"
 raw_data_file_name <- "meta_logs.rds"
 start_dates_file_name <- "study_dates.rds"
 static_features_file_name <- "static_features.rds"
