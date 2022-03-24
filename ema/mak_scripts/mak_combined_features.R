395d4b813d6689aaea617c0eef0ce456ec42276a
John Curtin <jjcurtin@wisc.edu>
2022-03-24 13:41:53 -0500

    Add files for parameterized mak_combined_features

---
+++ ema/mak_scripts/mak_combined_features.R
@@ -0,0 +1,15 @@
+library(here)
+library(stringr)
+
+data_type <- "ema"
+lead <- 0  # NOT YET USED.  WILL NEED TO UPDATE FILE NAMES WHEN USED
+# windows <- c("1hour", "1day", "1week")
+windows <- c("1day")
+
+for (window in windows) {
+  rmarkdown::render(input = here("shared/scripts_parameterized/mak_combined_features.Rmd"), 
+                    output_file = str_c("mak_combined_features_", data_type, "_", window, ".html"), 
+                    output_dir = str_c("P:/studydata/ema/knits/", data_type),
+                    params = list(window = window, data_type = data_type, lead = lead),
+                    envir = new.env())
+}
