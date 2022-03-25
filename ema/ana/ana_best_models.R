22898d584598df7608085bfbf52e8fd8f09cff63
jjcurtin <jjcurtin@wisc.edu>
2022-03-25 12:09:08 -0500

    Add lead

--- ema/ana/ana_best_models.R
+++ ema/ana/ana_best_models.R
@@ -3,11 +3,12 @@ library(stringr)
 
 data_type <- "ema"
 windows <- c("1hour", "1day", "1week")
+lead <- 0
 
 for (window in windows) {
   rmarkdown::render(input = here("shared/scripts_parameterized/ana_best_model.Rmd"), 
                     output_file = str_c("ana_best_model_", data_type, "_", window, ".html"), 
                     output_dir = str_c("P:/studydata/risk/knits/", data_type),
-                    params = list(window = window, data_type = data_type),
+                    params = list(window = window, data_type = data_type, lead = lead),
                     envir = new.env())
 }
