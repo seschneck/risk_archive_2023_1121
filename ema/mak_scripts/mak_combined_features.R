375e4a080509516ef0616bd0dcfc9296f63b2d09
jjcurtin <jjcurtin@wisc.edu>
2022-03-25 12:21:44 -0500

    Fix path bug

--- ema/mak_scripts/mak_combined_features.R
+++ ema/mak_scripts/mak_combined_features.R
@@ -5,12 +5,13 @@ data_type <- "ema"
 lead <- 0  # NOT YET USED.  WILL NEED TO UPDATE FILE NAMES WHEN USED
 # windows <- c("1hour", "1day", "1week")
 windows <- c("1day")
+version <- "v1"
 
 for (window in windows) {
   rmarkdown::render(input = here("shared/scripts_parameterized/mak_combined_features.Rmd"), 
                     output_file = str_c("mak_combined_features_", data_type, "_", window, ".html"), 
-                    output_dir = str_c("P:/studydata/risk/knits/ema/", data_type),
-                    params = list(window = window, data_type = data_type, lead = lead),
+                    output_dir = str_c("P:/studydata/risk/knits/", data_type),
+                    params = list(window = window, data_type = data_type, lead = lead, version = version),
                     envir = new.env())
 }
  
