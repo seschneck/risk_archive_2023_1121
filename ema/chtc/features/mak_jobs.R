e227c8fbff3a3a6e0a5778e836efc5d34df2eddd
John Curtin <jjcurtin@wisc.edu>
2022-03-27 21:16:23 -0500

    Update mak_jobs.R

--- ema/chtc/features/mak_jobs.R
+++ ema/chtc/features/mak_jobs.R
@@ -1,4 +1,11 @@
-# setup jobs to make EMA features on chtc 
+# setup jobs to make EMA features on CHTC 
+
+# EDIT THESE
+window <- "1day"   # remember to edit these in make_features_chtc.R as well
+data_type <- "ema"
+lead <- 0
+version <- "v2"
+
 
 # load packages
 library(here)
@@ -12,24 +19,20 @@ library(vroom)
 library(stringr)
 
 # Paths and filenames
-path_jobs <- "P:/studydata/risk/chtc/ema"
-path_ema <- "P:/studydata/risk/data_processed/ema" 
+path_jobs <- str_c("P:/studydata/risk/chtc/", data_type)
+name_job <- str_c("features_", window, "_", lead, "_", version)
+
+path_processed <- "P:/studydata/risk/data_processed/ema" 
 name_ema <- "ema.csv"
-path_lapses <- "P:/studydata/risk/data_processed/shared" 
-name_lapses <- "lapses.csv"
-name_labels <- "labels_1day.csv"
+name_labels <- str_c("labels_", window, ".csv")
 name_study_dates <- "study_dates.csv"
+
+path_shared <- "P:/studydata/risk/data_processed/shared" 
+name_lapses <- "lapses.csv"
+
 name_fun <- "fun_features.R"
 name_script <- "mak_features_chtc.R"
 
-window <- "1day"   # remember to edit these in make_features_chtc.R as well
-lead <- 0
-version <- "v2"
-
-
-###############################
-
-name_job <- str_c("features_", window, "_", lead, "_", version)
 
 # create new job directory (if it does not already exist) 
 if (!dir.exists(here(path_jobs, name_job))) {
@@ -38,14 +41,12 @@ if (!dir.exists(here(path_jobs, name_job))) {
   dir.create(here(path_jobs, name_job, "output"))
   dir.create(here(path_jobs, name_job, "output", "features"))
   dir.create(here(path_jobs, name_job, "output", "error"))
-  dir.create(here(path_jobs, name_job, "output", "out"))
-  
 } else {
   stop("Job folder already exists. No new folders created.")
 }
 
 # save out jobs csv file for queue
-n_jobs <- nrow(vroom(here(path_ema, name_labels), show_col_types = FALSE))
+n_jobs <- nrow(vroom(here(path_processed, name_labels), show_col_types = FALSE))
 labels_per_job <- 300
 job_start <- seq(1, n_jobs, by = labels_per_job) 
 job_stop <- c(seq(job_start[2] - 1, n_jobs, by = labels_per_job), n_jobs)
@@ -54,13 +55,13 @@ tibble(job_start, job_stop) %>%
               col_names = FALSE, append = FALSE)
 
 # select and format relevant variables and then copy ema
-vroom(here(path_ema, name_ema), show_col_types = FALSE) %>% 
+vroom(here(path_processed, name_ema), show_col_types = FALSE) %>% 
   select(-ema_type, -ema_1_1, -ema_1_2, -ema_1_3, -ema_1_4, -ema_1_5, -ema_1_6) %>% 
   arrange(subid, dttm_obs) %>% 
   vroom_write(here(path_jobs, name_job, "input", "ema.csv"), delim = ",")
 
 # select and format relevant variables and then copy lapses
-vroom(here(path_lapses, name_lapses), show_col_types = FALSE) %>% 
+vroom(here(path_shared, name_lapses), show_col_types = FALSE) %>% 
   filter(!exclude) %>% 
   select(subid, dttm_obs = lapse_start) %>% 
   arrange(subid, dttm_obs) %>% 
@@ -68,9 +69,9 @@ vroom(here(path_lapses, name_lapses), show_col_types = FALSE) %>%
   vroom_write(here(path_jobs, name_job, "input", "lapses.csv"), delim = ",")
 
 # copy over other data files verbatim
-file.copy(from = here(path_ema, name_labels),
+file.copy(from = here(path_processed, name_labels),
           to = here(path_jobs, name_job, "input", "labels.csv")) 
-file.copy(from = here(path_ema, name_study_dates),
+file.copy(from = here(path_processed, name_study_dates),
           to = here(path_jobs, name_job, "input", "study_dates.csv")) 
 
 # copy over function script
@@ -78,10 +79,10 @@ file.copy(from = here("shared", name_fun),
           to = here(path_jobs, name_job, "input", name_fun))
 
 # copy over unix files(run script, submit, pre, post files)
-file.copy(from = here("ema", "chtc", "features", "1day", "unix", c(list.files(here("ema", "chtc", "features", "1day", "unix")))),
+file.copy(from = here(data_type, "chtc", "features", "unix", c(list.files(here(data_type, "chtc", "features", "unix")))),
           to = here(path_jobs, name_job, "input"),
           recursive = TRUE)
 
 # copy R script
-file.copy(from = here("gps", "chtc", "features", "1day", name_script),
+file.copy(from = here(data_type, "chtc", "features", name_script),
           to = here(path_jobs, name_job, "input", name_script))
