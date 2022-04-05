09f45df159da79e1f87f6c99cacf2b281394f0bc
John Curtin <jjcurtin@wisc.edu>
2022-04-05 16:03:03 -0500

    Updates to best model

--- ema/ana/ana_best_models.R
+++ ema/ana/ana_best_models.R
@@ -10,7 +10,7 @@ version <- "v2"
 for (window in windows) {
   rmarkdown::render(input = here("shared/scripts_parameterized/ana_best_model.Rmd"), 
                     output_file = str_c("ana_best_model_", data_type, "_", 
-                                        window, "_", lead, "_", version, ".html"), 
+                                        window, "_", lead, "_", version, ".html"),  
                     output_dir = str_c("P:/studydata/risk/knits/", data_type),
                     params = list(window = window, data_type = data_type, 
                                   lead = lead, version = version),
