e6b88b68bef869ef8f5e616a97b2c7e855b7147b
Kendra Wyant <kpaquette2@wisc.edu>
2022-06-15 10:49:40 -0500

    updates meta log tibbles

--- meta/mak_scripts/mak_chtc_log_tibble.R
+++ meta/mak_scripts/mak_chtc_log_tibble.R
@@ -1,10 +1,8 @@
 library(here)
 library(stringr)
  
-# set parameters to pass in
+# set parameters
 job_path <- "P:/studydata/risk/chtc/meta/train_1day_0_v2_glmnet/"
-
-# set other parameters for rendering
 data_type <- "meta"
 # windows <- c("1hour", "1day", "1week")
 windows <- c("1day")
@@ -18,6 +16,7 @@ for (window in windows) {
                     output_file = str_c("chtc_log_", data_type, "_", 
                                         window, "_", lead, "_", version, "_", algorithm, ".html"),  
                     output_dir = str_c("P:/studydata/risk/knits/", data_type),
-                    params = list(job_path = job_path),
+                    params = list(job_path = job_path, window = window, lead = lead,
+                                  version = version, algorithm = algorithm),
                     envir = new.env())
 }
