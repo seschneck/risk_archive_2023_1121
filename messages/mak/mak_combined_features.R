e3314fdc4f01f4ac611dc7b8124c762e65c0a7b9
John Curtin <jjcurtin@wisc.edu>
2022-07-12 09:14:49 -0500

    Update to include study parameter

---
+++ messages/mak/mak_combined_features.R
@@ -0,0 +1,19 @@
+library(here)
+library(stringr)
+
+study <- "messages"
+data_type <- "ema"
+# windows <- c("1hour", "1day", "1week")
+windows <- c("1week")
+lead <- 0   # constant for now
+version <- "v4"   # update based on version of features
+
+for (window in windows) {
+  rmarkdown::render(input = here("shared/scripts_parameterized/mak_combined_features.Rmd"), 
+                    output_file = str_c("mak_combined_features_", data_type, "_", 
+                                        window, "_", lead, "_", version, ".html"), 
+                    output_dir = str_c("P:/studydata/risk/knits/", study),
+                    params = list(study = study, window = window, data_type = data_type, lead = lead, version = version),
+                    envir = new.env())
+}
+ 
