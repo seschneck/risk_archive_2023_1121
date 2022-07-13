1db81d1630fedbc79378d148af70e97ac6efa9d4
John Curtin <jjcurtin@wisc.edu>
2022-07-12 22:00:29 -0500

    Update mak_combined_features.R

--- messages/mak/mak_combined_features.R
+++ messages/mak/mak_combined_features.R
@@ -5,15 +5,16 @@ study <- "messages"
 data_type <- "gps"
 # windows <- c("1hour", "1day", "1week")
 windows <- c("1week")
-lead <- 0   # constant for now
-version <- "v1"   # update based on version of features
+lead <- 0   
+version <- "v1"   
 
 for (window in windows) {
   rmarkdown::render(input = here("shared/scripts_parameterized/mak_combined_features.Rmd"), 
                     output_file = str_c("mak_combined_features_", data_type, "_", 
                                         window, "_", lead, "_", version, ".html"), 
                     output_dir = str_c("P:/studydata/risk/knits/", study),
-                    params = list(study = study, window = window, data_type = data_type, lead = lead, version = version),
+                    params = list(study = study, window = window, 
+                                  data_type = data_type, lead = lead, version = version),
                     envir = new.env())
 }
  
