2caf3b2c7dcd0fc48e765ab364faadd2eedaf80f
Kendra Wyant <kpaquette2@wisc.edu>
2022-07-31 07:28:47 -0500

    meta ana best model

--- meta/parameterized_scripts/ana_best_model.R
+++ meta/parameterized_scripts/ana_best_model.R
@@ -1,18 +1,19 @@
 library(here)
 library(stringr)
- 
+
+study <- "meta" 
 data_type <- "meta"
 # windows <- c("1hour", "1day", "1week")
 windows <- c("1day")
 lead <- 0
-version <- "v1"
+version <- "v2"
 
 for (window in windows) {
-  rmarkdown::render(input = here("shared/scripts_parameterized//ana_best_model.Rmd"), 
+  rmarkdown::render(input = here("shared/scripts_parameterized/ana_best_model.Rmd"), 
                     output_file = str_c("ana_best_model_", data_type, "_", 
                                         window, "_", lead, "_", version, ".html"),  
                     output_dir = str_c("P:/studydata/risk/knits/", data_type),
                     params = list(window = window, data_type = data_type, 
-                                  lead = lead, version = version),
+                                  lead = lead, version = version, study = study),
                     envir = new.env())
 }
