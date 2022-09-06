a1ebdc097cfe24d461142710a6c8a0db8803f22a
John Curtin <jjcurtin@wisc.edu>
2022-09-06 12:48:12 -0500

    Update ana_best_models.R

--- messages/ana/ana_best_models.R
+++ messages/ana/ana_best_models.R
@@ -12,7 +12,7 @@ for (window in windows) {
   rmarkdown::render(input = here("shared/scripts_parameterized/ana_best_model.Rmd"), 
                     output_file = str_c("ana_best_model_", data_type, "_", 
                                         window, "_", lead, "_", version, ".html"),  
-                    output_dir = str_c("P:/studydata/risk/knits/", data_type),
+                    output_dir = str_c("P:/studydata/risk/knits/", study),
                     params = list(study = "messages", 
                                   window = window, 
                                   data_type = data_type, 
