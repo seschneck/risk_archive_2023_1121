cdfc225d8eba685c721724d136550d1eadaeb568
John Curtin <jjcurtin@wisc.edu>
2022-09-11 20:37:26 -0500

    Update mak_jobs.R

--- ema/chtc/training/mak_jobs.R
+++ ema/chtc/training/mak_jobs.R
@@ -3,6 +3,6 @@
 library(tidyverse)
 library(here)
 path_training_controls <- here("ema/chtc/training/training_controls_ema.R") 
-source(here("../lab_support/chtc/static_files/input/fun_chtc.R"))
+source(here("../lab_support/chtc/static_files/fun_chtc.R"))
 
-make_jobs(path_training_controls, overwrite_jobs = FALSE)
+make_jobs(path_training_controls, overwrite_jobs = TRUE)
