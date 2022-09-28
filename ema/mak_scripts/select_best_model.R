f7da785619cf8845003e7c4e13aaaeee6441cb45
Kendra Wyant <kpaquette2@wisc.edu>
2022-09-28 15:39:58 -0500

    updates select_best_model for nested and non-nested

---
+++ ema/mak_scripts/select_best_model.R
@@ -0,0 +1,20 @@
+library(here)
+library(stringr)
+
+study <- "ema"
+data_type <- "all"
+# windows <- c("1hour", "1day", "1week")
+windows <- c("1week")
+lead <- 0
+version <- "v4"
+cv <- "nested"
+
+for (window in windows) {
+  rmarkdown::render(input = here("shared/scripts_parameterized/select_best_model.Rmd"), 
+                    output_file = str_c("select_best_model", data_type, "_", 
+                                        window, "_", lead, "_", version, "_", cv, ".html"), 
+                    output_dir = str_c("P:/studydata/risk/knits/", study),
+                    params = list(window = window, study = study, data_type = data_type, 
+                                  lead = lead, version = version, cv = cv),
+                    envir = new.env())
+}
