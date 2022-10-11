c6681dca2efe022c9e9e79a5ce089cb7a255cb65
Kendra Wyant <kpaquette2@wisc.edu>
2022-10-11 09:12:30 -0500

    updates select_best_model as mak_best_model

---
+++ messages/mak/mak_best_model.R
@@ -0,0 +1,24 @@
+library(here)
+library(stringr)
+
+study <- "messages"
+data_type <- "all"
+cv <- "kfold"
+# windows <- c("1hour", "1day", "1week")
+windows <- c("1hour")
+lead <- 0
+version <- "v1"
+
+for (window in windows) {
+  rmarkdown::render(input = here("shared/scripts_parameterized/mak_best_model.Rmd"), 
+                    output_file = str_c("mak_best_model_", data_type, "_", 
+                                        window, "_", lead, "_", version,"_", cv, ".html"), 
+                    output_dir = str_c("P:/studydata/risk/knits/", study),
+                    params = list(study = study,
+                                  window = window, 
+                                  data_type = data_type, 
+                                  lead = lead, 
+                                  version = version,
+                                  cv = cv),
+                    envir = new.env())
+}
