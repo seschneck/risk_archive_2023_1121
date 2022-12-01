c50ec497847323f36d68a3767455be841f5e8beb
John Curtin <jjcurtin@wisc.edu>
2022-12-01 17:54:14 -0600

    Update for new version of lab_support

--- ema/chtc/training/mak_jobs.R
+++ ema/chtc/training/mak_jobs.R
@@ -5,4 +5,4 @@ library(here)
 path_training_controls <- here("ema/chtc/training/training_controls_ema.R") 
 source(here("../lab_support/chtc/static_files/fun_chtc.R"))
 
-make_jobs(path_training_controls, overwrite_jobs = TRUE)
+make_jobs(path_training_controls, overwrite_jobs = FALSE)
