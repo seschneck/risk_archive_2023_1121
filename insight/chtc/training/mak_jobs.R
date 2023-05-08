abe406320159a4c2958453cb3b6292839b91a62e
Kendra Wyant <kpaquette2@wisc.edu>
2023-05-08 10:52:22 -0500

    initial commit of insight

---
+++ insight/chtc/training/mak_jobs.R
@@ -0,0 +1,8 @@
+# setup chtc jobs & associated files/folders
+
+library(tidyverse)
+library(here)
+path_training_controls <- here("ema/chtc/training/training_controls_ema.R") 
+source(here("../lab_support/chtc/static_files/fun_chtc.R"))
+
+make_jobs(path_training_controls, overwrite_jobs = FALSE)
---
+++ insight/mak_scripts/mak_training_metrics.R
@@ -0,0 +1,21 @@
+library(here)
+library(stringr)
+
+study <- "ema"
+data_type <- "all"
+# windows <- c("1hour", "1day", "1week")
+windows <- c("1hour", "1day", "1week")
+lead <- 0
+version <- "v4"
+cv <- "kfold"
+algorithms <- "all"  # "all" or algorithm name
+
+for (window in windows) {
+  rmarkdown::render(input = here("shared/scripts_parameterized/mak_training_metrics.Rmd"), 
+                    output_file = str_c("mak_training_metrics_", data_type, "_", 
+                                        window, "_", lead, "_", version, "_", cv, ".html"), 
+                    output_dir = str_c("P:/studydata/risk/knits/", study),
+                    params = list(window = window, study = study, data_type = data_type, 
+                                  lead = lead, version = version, cv = cv),
+                    envir = new.env())
+}
