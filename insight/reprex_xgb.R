98f506ae3c69de558d1170b2482534ee55797f40
gfronk <gfronk@wisc.edu>
2023-07-27 14:36:16 -0500

    Create reprex_xgb.R

---
+++ insight/reprex_xgb.R
@@ -0,0 +1,35 @@
+library(tidymodels)
+library(xgboost)
+
+d <- tidyr::tibble(x1 = stats::runif(100, min = 0, max = 10),
+                   x2 = stats::runif(100, min = 0, max = 10),
+                   y = sample(c("yes", "no"), size = 100, replace = TRUE)) %>% 
+  dplyr::mutate(y = factor(y))
+
+head(d)
+
+# fit model using both predictor variables
+model_both <- boost_tree(learn_rate = 0.1,
+                         tree_depth = 2,
+                         mtry = 2,
+                         trees = 100,
+                         stop_iter = 20) %>% 
+  set_engine("xgboost",
+             validation = 0.2) %>% 
+  set_mode("classification") %>%
+  fit(y ~ ., data = d)
+
+summary(model_both)
+
+# fit model using only 1 predictor variable
+model_x1 <- boost_tree(learn_rate = 0.1,
+                       tree_depth = 2,
+                       mtry = 2,
+                       trees = 100,
+                       stop_iter = 20) %>% 
+  set_engine("xgboost",
+             validation = 0.2) %>% 
+  set_mode("classification") %>%
+  fit(y ~ x1, data = d)
+
+reprex::reprex(venue = "gh")
