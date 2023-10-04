7f1760c71f3e8fb0779ebb6eb826fc24dd6d7bf3
John Curtin <jjcurtin@wisc.edu>
2023-10-04 11:28:04 -0500

    remove old folders from messages grant

--- messages/chtc/training/mak_jobs.R
+++
@@ -1,8 +0,0 @@
-# setup chtc jobs & associated files/folders
-
-library(tidyverse)
-library(here)
-path_training_controls <- here("messages/chtc/training/training_controls_messages.R") 
-source(here("../lab_support/chtc/static_files/fun_chtc.R"))
-
-make_jobs(path_training_controls, overwrite_jobs = FALSE)
